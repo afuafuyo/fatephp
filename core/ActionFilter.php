ad9b3316d820b03b3276a708a7e08ae6f1f0c805
afu <yulifu@douyu.tv>
2022-04-12 11:57:33 +0800

    change js

--- core/ActionFilter.php
+++ core/ActionFilter.php
@@ -17,6 +17,12 @@ class ActionFilter extends Behavior {
     public function __construct() {
         $this->callbacks = [
             Controller::EVENT_BEFORE_ACTION => function($actionEvent) {
+                // 如果前一个 valid 为 false 那么本次 filter 不再执行
+                if(!$actionEvent->valid) {
+                    $this->unListen();
+                    return;
+                }
+
                 $this->beforeAction($actionEvent);
 
                 if(!$actionEvent->valid) {
